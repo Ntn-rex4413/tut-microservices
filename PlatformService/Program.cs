bdd012a6ec5323029dad18c1d5b8a84bef348142
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-04-05 19:26:03 +0200

    Scaffold PlatformService webapi, set up model and temporarily in-memory db context

---
+++ PlatformService/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => 
+    opt.UseInMemoryDatabase("InMem"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("api/v1/hello", () =>
+{
+
+    return Results.Ok("hello");
+});
+
+app.Run();
