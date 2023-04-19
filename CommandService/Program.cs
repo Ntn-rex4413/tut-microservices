421e4e735e99f6f7f86a2ab28a49b16b10593c5c
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-04-19 17:45:02 +0200

    Add interface and concrete class for processing message bus events + register for DI

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,4 +1,5 @@
 using CommandService.Data;
+using CommandService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 
 var app = builder.Build();
 
